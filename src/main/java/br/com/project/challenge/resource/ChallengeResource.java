52305641abb8374a3b0f66427377cd7f29efe85f
Bruno Yuji Canfora <bruno.canfora@gmail.com>
2018-03-07 17:20:29 -0300

    Alterações e criação de webservice

--- src/main/java/br/com/project/challenge/resource/ChallengeResource.java
+++ src/main/java/br/com/project/challenge/resource/ChallengeResource.java
@@ -1,9 +1,16 @@
 package br.com.project.challenge.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.project.challenge.model.Consulta;
+import br.com.project.challenge.model.Consultorio;
+import br.com.project.challenge.model.Medico;
+import br.com.project.challenge.repository.ConsultaRepository;
 import br.com.project.challenge.repository.ConsultorioRepository;
 import br.com.project.challenge.repository.MedicoRepository;
 
@@ -16,4 +23,22 @@ public class ChallengeResource {
 	
 	@Autowired
 	private ConsultorioRepository consultorioRepository;
+	
+	@Autowired
+	private ConsultaRepository consultaRepository;
+	
+	@GetMapping("/medico")
+	public List<Medico> listarMedico(){
+		return medicoRepository.findAll();
+	}
+	
+	@GetMapping("/consultorio")
+	public List<Consultorio> listarConsultorio(){
+		return consultorioRepository.findAll();
+	}
+	
+	@GetMapping("/consulta")
+	public List<Consulta> listarConsulta(){
+		return consultaRepository.findAll();
+	}
 }
