c039f64414210af19a3b62a5e28bd55715a76e6a
Bruno Yuji Canfora <bruno.canfora@gmail.com>
2018-03-08 10:51:15 -0300

    Alterações

--- src/main/java/br/com/project/challenge/repository/ConsultaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.project.challenge.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.project.challenge.model.Consulta;
-
-public interface ConsultaRepository extends JpaRepository<Consulta, Long>{
-
-}
--- src/main/java/br/com/project/challenge/resource/ChallengeResource.java
+++ src/main/java/br/com/project/challenge/resource/ChallengeResource.java
@@ -1,16 +1,21 @@
 package br.com.project.challenge.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.project.challenge.model.Consulta;
 import br.com.project.challenge.model.Consultorio;
 import br.com.project.challenge.model.Medico;
-import br.com.project.challenge.repository.ConsultaRepository;
 import br.com.project.challenge.repository.ConsultorioRepository;
 import br.com.project.challenge.repository.MedicoRepository;
 
@@ -24,8 +29,6 @@ public class ChallengeResource {
 	@Autowired
 	private ConsultorioRepository consultorioRepository;
 	
-	@Autowired
-	private ConsultaRepository consultaRepository;
 	
 	@GetMapping("/medico")
 	public List<Medico> listarMedico(){
@@ -37,8 +40,14 @@ public class ChallengeResource {
 		return consultorioRepository.findAll();
 	}
 	
-	@GetMapping("/consulta")
-	public List<Consulta> listarConsulta(){
-		return consultaRepository.findAll();
+	@PostMapping("/salvarMedico")
+	public ResponseEntity<Medico> criar(@RequestBody Medico medico, HttpServletResponse response) {
+		Medico medicoSalva = medicoRepository.save(medico);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(medicoSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(medicoSalva);
 	}
 }
